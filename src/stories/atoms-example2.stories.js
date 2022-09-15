9bc5ee49fffe05a5571465e5174bfc42907c56f5
Ozawa Sachio <s.ozawa@4nonome.com>
2022-09-15 20:56:25 +0900

    ローカルからリモートのorigin/masterにstoriesフォルダの全てを反映

---
+++ src/stories/atoms-example2.stories.js
@@ -0,0 +1,6 @@
+import Item_Example2 from "../components/atoms/example/example2.html";
+import "../components/atoms/example/example2.scss";
+
+export default { title: "Atoms/Example2" };
+
+export const Example2 = () => Item_Example2;
