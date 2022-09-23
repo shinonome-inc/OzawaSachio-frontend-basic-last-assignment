6b1325dc731ce734a7cdba045ea9a2066b6371eb
Ozawa Sachio <s.ozawa@4nonome.com>
2022-09-23 21:11:45 +0900

    storiesをgithubにpush

---
+++ src/stories/organisms-news.stories.js
@@ -0,0 +1,6 @@
+import Organisms_News from "../components/organisms/news/news.html";
+import "../components/organisms/news/news.scss";
+
+export default { title: "Organisms/News" };
+
+export const News = () => Item_News;
