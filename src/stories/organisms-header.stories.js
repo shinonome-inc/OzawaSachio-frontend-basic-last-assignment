0eb36d9d5e88fc0aeb61a0fea60c9b457646dfb3
Ozawa Sachio <s.ozawa@4nonome.com>
2022-09-25 16:31:07 +0900

    .stories.jsファイルをorganisms-headerにpush

---
+++ src/stories/organisms-header.stories.js
@@ -0,0 +1,6 @@
+import Item_Header from "../components/organisms/header/header.html";
+import "../components/organisms/header/header.scss";
+
+export default { title: "Organisms/Header" };
+
+export const Header = () => Item_Header;
