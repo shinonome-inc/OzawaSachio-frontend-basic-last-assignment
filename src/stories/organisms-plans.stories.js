1916de28cb12226e377a3cb9e39b9f6127fd16ca
Ozawa Sachio <s.ozawa@4nonome.com>
2022-11-20 12:57:00 +0900

    organisms-plans first commit

---
+++ src/stories/organisms-plans.stories.js
@@ -0,0 +1,6 @@
+import Item_Plans from "../components/organisms/plans/plans.html";
+import "../components/organisms/plans/plans.scss";
+
+export default { title: "Organisms/Plans" };
+
+export const Plans = () => Item_Plans;
