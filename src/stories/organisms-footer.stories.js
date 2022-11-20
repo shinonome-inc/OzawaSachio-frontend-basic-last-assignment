811be1612d681fc1ccf0e7d53e20b43b13ad8cc3
Ozawa Sachio <s.ozawa@4nonome.com>
2022-11-20 17:14:04 +0900

    organisms-footer first commit

---
+++ src/stories/organisms-footer.stories.js
@@ -0,0 +1,6 @@
+import Item_Footer from "../components/organisms/footer/footer.html";
+import "../components/organisms/footer/footer.scss";
+
+export default { title: "Organisms/Footer" };
+
+export const Footer = () => Item_Footer;
