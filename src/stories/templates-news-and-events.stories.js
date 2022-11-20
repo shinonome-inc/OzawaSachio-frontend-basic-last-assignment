a8c311e5a3c76029b943fd8f85ad0ee489af6147
Ozawa Sachio <s.ozawa@4nonome.com>
2022-11-20 18:23:02 +0900

    templates-newsandeventsをgithubに作成

---
+++ src/stories/templates-news-and-events.stories.js
@@ -0,0 +1,6 @@
+import Item_NewsAndEvents from "../components/templates/news-and-events/news-and-events.html";
+import "../components/templates/news-and-events/news-and-events.scss";
+
+export default { title: "Templates/NewsAndEvents" };
+
+export const NewsAndEvents = () => Item_NewsAndEvents;
